0124971cdf430a094988f0ed46d8f341d4d2b84f
xxicypatxx <49638074+xxicypatxx@users.noreply.github.com>
2021-02-17 15:34:54 +1100

    Uploaded solutions

---
+++ Tree/105_solution.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        if not preorder:
+            return None
+
+        root = TreeNode(preorder[0])
+        idx = inorder.index(root.val)
+
+        root.left = self.buildTree(preorder[1:idx + 1], inorder[:idx])
+        root.right = self.buildTree(preorder[idx + 1:], inorder[idx + 1:])
+
+        return root
