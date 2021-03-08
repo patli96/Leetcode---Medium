0b26fdeeceaf9e19b57e0f424381f8fe533768e0
xxicypatxx <49638074+xxicypatxx@users.noreply.github.com>
2021-03-08 17:38:23 +1100

    Uploaded images

--- Algorithms/sorting_algorithms.py
+++ Algorithms/sorting_algorithms.py
@@ -1,5 +1,6 @@
 import random
 
+''' Bubble Sort'''
 def bubbleSort(arr):
     n = len(arr)
     for i in range(n):
@@ -9,6 +10,7 @@ def bubbleSort(arr):
     return arr
 
 
+''' Select Sort'''
 def selectionSort(arr):
     n = len(arr)
     for i in range(n):
@@ -20,6 +22,7 @@ def selectionSort(arr):
     return arr
 
 
+''' Insertion Sort'''
 def insertionSort(arr):
     n = len(arr)
     for i in range(1, n):
@@ -32,6 +35,7 @@ def insertionSort(arr):
     return arr
 
 
+''' Merge Sort'''
 def mergeSort(arr):
     n = len(arr)
     if n < 2:
@@ -52,6 +56,7 @@ def mergeSort(arr):
     return ret
 
 
+''' Quick Sort'''
 def quickSort(arr):
     n = len(arr)
     if n < 2:
@@ -64,6 +69,41 @@ def quickSort(arr):
     return ret
 
 
+''' Heap Sort'''
+def heapify(arr, n, i):
+    max_val = i # initialize the largest value as root
+    l = 2 * i + 1   # left child idx
+    r = 2 * i + 2   # right child idx
+
+    # See if left child of root exists and is greater than the root
+    if l < n and arr[i] < arr[l]:
+        max_val = l
+    # See if right child of root exists and is greater than the root
+    if r < n and arr[max_val] < arr[r]:
+        max_val = r
+
+    # swap the root, if needed
+    if max_val != i:
+        arr[i], arr[max_val] = arr[max_val], arr[i]
+
+        # heapify the root
+        heapify(arr, n, max_val)
+
+
+
+def heapSort(arr):
+    n = len(arr)
+
+    # build a max-heap
+    for i in range(n, -1, -1):
+        heapify(arr, n, i)
+    # swap elements
+    for i in range(n-1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+
+
 a = [random.randint(0, 100) for i in range(20)]
 print(a)
-print(quickSort(a))
+heapSort(a)
+print(a)
